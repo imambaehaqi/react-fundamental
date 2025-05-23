9cd7efe707b30a4cd7e172b934953d950a76a045
Imam Baehaqi <mail.baehaqi@gmail.com>
2025-05-23 13:28:41 +0700

    introduction jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,42 @@
 import "./App.css";
 import Article from "./components/Article";
+import ProductItem from "./components/ProductItem";
 
 function App() {
+  // Membuat variable yang berisi tentang array object
+  // const data = [
+  //   {
+  //     namaProduk: "Yupi",
+  //     harga: "500",
+  //     deskripsi: "permen",
+  //   },
+  //   {
+  //     namaProduk: "Kapas",
+  //     harga: "1000",
+  //     deskripsi: "Tumbuhan",
+  //   },
+  // ];
+
   return (
     <>
       <div className="App">
-        <Article /> {/* Komponen bisa dipanggil disini */}
+        {/** Perulangan untuk menampilkan array */}
+        {/* {data.map((d) => {
+          return (
+            <ProductItem
+              namaProduk={d.namaProduk}
+              harga={d.harga}
+              deskripsi={d.deskripsi}
+            />
+          );
+        })} */}
+        {/** Cara hardcode untuk memanggil component dari parameter */}
+        {/* <ProductItem namaProduk="Yupi" harga="500" deskripsi="Permen" />
+        <ProductItem namaProduk="Yupi" harga="500" deskripsi="Permen" />
+        <ProductItem namaProduk="Yupi" harga="500" deskripsi="Permen" />
+        <ProductItem namaProduk="Yupi" harga="500" deskripsi="Permen" />
+        <ProductItem namaProduk="Yupi" harga="500" deskripsi="Permen" /> */}
+        <Article /> {/** Komponen dipanggil */}
       </div>
     </>
   );
--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -3,7 +3,29 @@
 }
 
 function Article() {
-  return <div>Ini adalah artikel satu, dua dan tiga</div>;
+  const name = "Hello, everybody";
+  const titles = ["React.js", "Next.js", "Node.js"];
+
+  return (
+    <>
+      <div>{name}</div>
+      <div>
+        {/* Return untuk pemanggilan satu */}
+        {titles.map((title) => {
+          <div>{title}</div>;
+        })}
+        {/* Return untuk pemanggilan banyak */}
+        {titles.map((title) => {
+          return (
+            <>
+              <div>{title}</div>
+              <div>{title}</div>
+            </>
+          );
+        })}
+      </div>
+    </>
+  );
 }
 
 export default Article;
